fb687441b12ef3d5ee1dbac266b1bb187567cbe4
Laura Zheng <lyzheng@umd.edu>
2024-06-21 01:09:06 +0100

    updated footer

--- build.py
+++ build.py
@@ -1,9 +1,11 @@
 import os
 import glob
 import markdown
-from xml.dom import minidom
+import datetime
 
 layout_html = "content/_layout.html"
+timestamp = datetime.datetime.now().strftime("%Y年%m月%d日")
+footer_msg = f"Last updated on {timestamp}"
 
 # main navbar pages
 for f in glob.iglob('content/*.md'):
@@ -19,9 +21,12 @@ for f in glob.iglob('content/*.md'):
 
     layout_contents = layout_contents.split("MAIN_INFO_HERE")
     assert len(layout_contents) == 2
+    file_content = html.join(layout_contents)
+    layout_contents = file_content.split("FOOTER_MSG_HERE")
+    assert len(layout_contents) == 2
+    file_content = footer_msg.join(layout_contents)
     
     with open(destination, 'w') as file:
         print(f"writing to {destination}")
-        file_content = html.join(layout_contents)
         file.write(str(file_content))
         
